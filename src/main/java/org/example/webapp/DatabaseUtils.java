681c7c92cab480ffa2c63eae7d791eed7b7ab550
Ricki Angel <rickihippy@hotmail.com>
2024-07-30 20:33:42 +0100

    Updated pom.xml, added images to rep

--- src/main/java/org/example/webapp/DatabaseUtils.java
+++ src/main/java/org/example/webapp/DatabaseUtils.java
@@ -8,8 +8,12 @@ public class DatabaseUtils {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";
     private static final String USER = "system";
     private static final String PASSWORD = "duda123";
+    private static Connection connection = null;
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
     }
 }
